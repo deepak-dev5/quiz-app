64e11ec27c5ab37554bf581714a9c12453afed73
Deepak Singh <deepakkrsingh196@gmail.com>
2025-12-04 20:13:43 +0530

    Fix: Allow Cross origin

--- quiz-service/src/main/java/com/assessment/quiz_service/controller/QuizController.java
+++ quiz-service/src/main/java/com/assessment/quiz_service/controller/QuizController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/quizzes")
+@CrossOrigin(origins = "http://localhost:3000")
 public class QuizController {
 
     private final QuizService quizService;
